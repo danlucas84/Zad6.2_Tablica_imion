c3ed27c603fc32a04236255cd046ee98bc1db150
Dell <dandan_21@o2.pl>
2019-05-20 21:15:36 +0200

    proste rozwiazanie zad bez array to string

---
+++ src/TablicaImion.java
@@ -0,0 +1,17 @@
+public class TablicaImion {
+
+    public static void main(String[] args) {
+
+
+        String [] names = new String [3];
+
+        names[0] = "Kasia";
+        names[1] = "Asia";
+        names[2] = "Basia";
+
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+
+    }
+}
